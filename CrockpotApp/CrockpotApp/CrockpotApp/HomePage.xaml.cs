0a1798fab5afcdb49faa87d3d7762e4dcc5ecc74
TimProctorAUT <42375425+TimProctorAUT@users.noreply.github.com>
2018-08-29 10:22:30 +1200

    Adding All Xamarin Project Files
    Adding the Crockpot Project files for Visual Studio

---
+++ CrockpotApp/CrockpotApp/CrockpotApp/HomePage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CrockpotApp
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
